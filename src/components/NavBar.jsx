a02f9708612901d62a7196456c901813ff1c958d
Kevin-Krantz <kevinkrantz95@hotmail.com>
2022-07-25 20:36:58 +0200

    Changed NavBar component to a stateless function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <span className="navbar-brand h1">
-            Navbar
-            <span className="badge rounded-pill bg-secondary ms-2">
-              {this.props.productCount}
-            </span>
+function NavBar(props) {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand h1">
+          Navbar
+          <span className="badge rounded-pill bg-secondary ms-2">
+            {props.productCount}
           </span>
-        </div>
-      </nav>
-    );
-  }
+        </span>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Product from "./Product";
 
-function Products(props) {
-  const { onReset, onDelete, onIncrement, onDecrement, products } = this.props;
-
+function Products({ onReset, onDelete, onIncrement, onDecrement, products }) {
   return (
     <div className="container mt-4">
       <button onClick={onReset} className="btn btn-primary">
         Reset
       </button>
-      {this.props.products.map((product) => (
+      {products.map((product) => (
         <Product
           key={product.id}
           product={product}
